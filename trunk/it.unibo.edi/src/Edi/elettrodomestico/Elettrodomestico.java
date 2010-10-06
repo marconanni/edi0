8750fcbd46edbdf27a1b4329fc779e06e0e76e06
marco.nanni0 <marco.nanni0@74a1bea1-40ad-62f2-e98e-8becefb7c64b>
2010-10-06 10:25:23 +0000

    Sviluppata parte della classe elettrodomestico, mancono i metodi accendi, spegni, disattiva e riattiva.

--- trunk/it.unibo.edi/src/Edi/elettrodomestico/Elettrodomestico.java
+++ trunk/it.unibo.edi/src/Edi/elettrodomestico/Elettrodomestico.java
@@ -20,8 +20,51 @@ public class Elettrodomestico {
 		this.oraAccensione = oraAccensione;
 	}
 	
+	public boolean isOn(){
+		return isAvvio()||isEsercizio();
+	}
+	public boolean isAvvio(){
+		return this.stato==StatoElettrodomestico.avvio;
+	}
+	public boolean isEsercizio(){
+		return this.stato==StatoElettrodomestico.esercizio;
+	}
+	public boolean isOff(){
+		return this.isDisattivato()||stato==StatoElettrodomestico.spento;
+	}
 	
+	public boolean isDisattivato(){
+		return this.stato==StatoElettrodomestico.disattivato;
+	}
 	
+	public StatoElettrodomestico getStato(){
+		return this.stato;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getConsumoEsercizio() {
+		return consumoEsercizio;
+	}
+
+	public int getConsumoAvvio() {
+		return consumoAvvio;
+	}
+
+	public Date getOraAccensione() {
+		return oraAccensione;
+	}
 	
+	public int getConsumoAttuale(){
+		if(this.stato==StatoElettrodomestico.avvio)
+			return this.consumoAvvio;
+		else 
+			if(this.stato==StatoElettrodomestico.esercizio)
+				return this.consumoEsercizio;
+			else		
+				return 0;
+	}
 
 }
