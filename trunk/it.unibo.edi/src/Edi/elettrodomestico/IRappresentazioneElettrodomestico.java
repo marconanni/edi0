c40159846acbaff167f4c461973761ec004e4928
marco.nanni0 <marco.nanni0@74a1bea1-40ad-62f2-e98e-8becefb7c64b>
2010-10-19 15:33:32 +0000

    continuo a sviluppare i metodi privati e pubblici di scontrol: mancano le azione legate alla ricezione ed invio di messaggi di contace e la fase di costruzione e configurazione iniziale

--- trunk/it.unibo.edi/src/Edi/elettrodomestico/IRappresentazioneElettrodomestico.java
+++ trunk/it.unibo.edi/src/Edi/elettrodomestico/IRappresentazioneElettrodomestico.java
@@ -1,5 +1,7 @@
 package Edi.elettrodomestico;
 
+import java.util.Date;
+
 public interface IRappresentazioneElettrodomestico {
 
 	/**
@@ -17,14 +19,15 @@ public interface IRappresentazioneElettrodomestico {
 	/**
 	 * 
 	 * @return il consumo attale dell'elettrodomestico ( se acceso l'ultimo consumo registrato, se spento 0)
+	 *	se l'elettrodomestico è disattivato contiene l'ultimo dato di consumo ricevuto
 	 */
-	public int getConsumoAttuale();
+	public int getConsumo();
 
 	/**
 	 * 
 	 * @param consumoAttuale il consumo attale dell'elettrodomestico ( se acceso l'ultimo consumo registrato, se spento 0)
 	 */
-	public void setConsumoAttuale(int consumoAttuale);
+	public void setConsumo(int consumoAttuale);
 
 	/**
 	 * 
@@ -43,5 +46,17 @@ public interface IRappresentazioneElettrodomestico {
 	 * @return l'identificativo dell'elettrodomestico.
 	 */
 	public String getId();
+
+	/**
+	 * 
+	 * @return l'ora dell'ultima accensione dell'elettrodomestico
+	 */
+	public Date getOraAccensione();
+
+	/**
+	 * 
+	 * @param oraAccensione: l'ora dell'ultima accensione dell'elettrodomestico
+	 */
+	public void setOraAccensione(Date oraAccensione);
 
 }
