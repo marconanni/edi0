c0fa73cd1f9dea0cd9a3bd2a1ce0bc3308cffd93
marco.nanni0 <marco.nanni0@74a1bea1-40ad-62f2-e98e-8becefb7c64b>
2010-11-01 18:28:32 +0000

    FAtti i test dell'interruttore.

--- trunk/it.unibo.edi/src/test/scontrol/InterruttoreTest.java
+++ trunk/it.unibo.edi/src/test/scontrol/InterruttoreTest.java
@@ -1,4 +1,7 @@
 package test.scontrol;
+import Edi.scontrol.*;
+import Edi.elettrodomestico.Elettrodomestico;
+import Edi.elettrodomestico.StatoElettrodomestico;
 
 /**
  * @author Marco
@@ -6,6 +9,8 @@ package test.scontrol;
  * @created 28-ott-2010 18:54:47
  */
 public class InterruttoreTest extends junit.framework.TestCase {
+	private Interruttore interruttore;
+	private Elettrodomestico elettrodomestico;
 
 	public InterruttoreTest(){
 
@@ -37,6 +42,9 @@ public class InterruttoreTest extends junit.framework.TestCase {
 	protected void setUp()
 	  throws Exception{
 		super.setUp();
+		this.elettrodomestico= new Elettrodomestico(StatoElettrodomestico.spento, "e1", 60, 120, null);
+		this.interruttore = new Interruttore("i1");
+		interruttore.setElettrodomesticoCollegato(elettrodomestico);
 	}
 
 	/**
@@ -48,7 +56,45 @@ public class InterruttoreTest extends junit.framework.TestCase {
 		super.tearDown();
 	}
 
-	public final void testInterruttore(){
-
+	public void testTurnOn(){
+		this.interruttore.turnOn();
+		assertTrue(interruttore.isOn());
+		assertTrue(elettrodomestico.isOn());
+		assertTrue(elettrodomestico.isAvvio());
 	}
+	public void testTurnOff(){
+		this.testTurnOn();
+		this.interruttore.turnOff();
+		assertTrue(interruttore.isOff());
+		assertFalse(interruttore.isOn());
+		assertTrue(elettrodomestico.isSpento());
+	}
+	
+	public void testIsOn(){
+		interruttore.turnOn();
+		assertTrue(interruttore.isOn());
+	}
+	public void testIsOff(){
+		interruttore.turnOff();
+		assertTrue(interruttore.isOff());
+	}
+	public void testIsCollegato(){
+		interruttore.setElettrodomesticoCollegato(null);
+		assertFalse(interruttore.isCollegato());
+		interruttore.setElettrodomesticoCollegato(elettrodomestico);
+		assertTrue(interruttore.isCollegato());
+	}
+	public void testGetId(){
+		assertEquals("i1",interruttore.getId());
+	}
+	public void testGetIdElettrodomesticoCollegato(){
+		assertEquals(elettrodomestico.getId(),interruttore.getIdElettrodomesticoCollegato());
+	}
+	public void testSetElettrodomesticoCollegato(){
+		Elettrodomestico nuovoElettrodomestico = new Elettrodomestico(StatoElettrodomestico.spento, "e2", 60, 120, null);
+		interruttore.setElettrodomesticoCollegato(nuovoElettrodomestico);
+		assertEquals(nuovoElettrodomestico.getId(),interruttore.getIdElettrodomesticoCollegato());
+		
+	}
+	
 }//end InterruttoreTest
